433cc69a22fe5c74f739f4cb13317975951f0547
Simon <phillipe.simon@gmail.com>
2025-02-25 22:06:26 -0400

    NLW Connect - Aula 01

---
+++ Trilha FullStack/app.js
@@ -0,0 +1,21 @@
+const app = document.getElementById("app");
+
+const formAction = () => {
+  const form = document.getElementById("form");
+  form.onsubmit = (event) => {
+    event.preventDefault();
+  };
+};
+
+const startApp = () => {
+  const content = `
+    <form id="form">
+    <input type="email" name="email" placeholder="e-mail">
+    <input type="text" name="phone" placeholder="telefone">
+    <button>Confirmar</button>
+  </form>
+    `;
+  app.innerHTML = content;
+  formAction();
+};
+startApp();
