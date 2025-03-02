d1a2b24da0074c27c76a867eb54fd37e9e2a3f9b
Simon <phillipe.simon@gmail.com>
2025-03-02 17:26:32 -0400

    NLW-Connect Aula 03. Evento Finalizado!

--- Trilha FullStack/app.js
+++ Trilha FullStack/app.js
@@ -35,18 +35,29 @@ const getTotalSubscribers = (userData) => {
 
 const showInvite = (userData) => {
   app.innerHTML = `
-  <input type="text" id="link" value="https://evento.com?ref=${
-    userData.ref
-  }" disabled>
-  <div id="stats">
-  <h4>
-  ${getTotalSubscribers(userData)} subscribers
-  </h4>
-  <p>
-  Inscrições feitas
-  </p>
-  </div>
+   <main>
+        <h3>Inscrição confirmada!</h3>
+
+        <p>
+          Convide mais pessoas e concorra a prêmios! <br />Compartilher o link e
+          acompanhe as inscrições:
+        </p>
+        <div class="input-group">
+          <label for="link">
+            <img src="link.svg" alt="Link icon" />
+          </label>
+          <input type="text" id="link" value="https://example.com?ref=${
+            userData.ref
+          }" disabled />
+        </div>
+      </main>
+      <section class="stats">
+        <h4>${getTotalSubscribers(userData)}</h4>
+        <p>Inscrições feitas</p>
+      </section>
   `;
+  app.setAttribute("class", "page-invite");
+  updateImageLinks();
 };
 
 const saveUser = (userData) => {
@@ -80,17 +91,69 @@ const formAction = () => {
   };
 };
 
+const updateImageLinks = () => {
+  document.querySelectorAll("img").forEach((img) => {
+    if (img.src.includes("githubusercontent")) {
+      return;
+    }
+    const src = img.getAttribute("src");
+    img.src = `https://raw.githubusercontent.com/maykbrito/my-public-files/main/nlw-19/${src}`;
+  });
+};
+
 const startApp = () => {
   const content = `
-    <form id="form">
-    <input type="email" name="email" placeholder="e-mail">
-    <input type="text" name="phone" placeholder="telefone">
-    <button>Confirmar</button>
-  </form>
-    `;
+  
+          <main>
+            <section class="about">
+                <div class="section-header">
+                  <h2>Sobre o evento</h2>
+                  <span class="badge">Ao vivo</span>
+                </div>
+  
+                <p>
+                  Um evento feito por e para pessoas desenvolvedoras 
+                  apaixonadas por criar soluções inovadoras e compartilhar conhecimento.
+                   Vamos mergulhar nas tendências mais recentes em desenvolvimento de 
+                   software, arquitetura de sistemas e tecnologias emergentes, 
+                   com palestras, workshops e hackathons.
+                  <br/><br/>
+  Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito 
+                </p>
+            </section>
+            <section class="registration">
+              <h2>Inscrição</h2>
+                <form id="form">
+                  <div class="input-wrapper">
+                    <div class="input-group">
+                      <label for="email">
+                        <img src="mail.svg" alt="Email icon">
+                      </label>
+                      <input type="email" id="email" name="email" placeholder="E-mail">
+                      </div>
+                  
+                  <div class="input-group">
+                      <label for="phone">
+                        <img src="phone.svg" alt="Phone icon">
+                      </label>
+                      <input type="phone" id="phone" name="phone" placeholder="Telefone">
+                      </div>
+                    </div>  
+  
+                    <button>
+                      Confirmar
+                      <img src="arrow.svg" alt="Arrow right">
+                    </button>
+                </form>
+            </section>
+          </main>
+   `;
+
   app.innerHTML = content;
+  app.setAttribute("class", "page-start");
+  updateImageLinks();
   formAction();
 };
 startApp();
 
-document.getElementById("logo").onclick = () => startApp();
+document.querySelector("header").onclick = () => startApp();
